4c4cbdaae6d3d57546afc4a2289cfa3e3f90e58d
zhaoqiangq <18235813489@163.com>
2019-08-08 09:23:47 +0800

    index succee

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if(res.code){
+          wx.request({
+            url: 'http://italwebapi.86sb.com/gzynew/openid',
+            data:{
+              code:res.code
+            },
+            success(res){
+              console.log(res)
+            }
+          })
+        }
       }
     })
     // 获取用户信息
