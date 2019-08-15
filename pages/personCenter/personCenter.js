992c1940ce760622bdfaf5863e2e284b5b60c866
changwenrui <906782507@qq.com>
2019-08-15 15:08:45 +0800

    个人中心头像调试

--- pages/personCenter/personCenter.js
+++ pages/personCenter/personCenter.js
@@ -39,32 +39,31 @@ Page({
         iconTitle: "开票信息"
       }
     ],
-    isLogin: true,
     telnumber: 15112345678
   },
 
   onLoad: function() {
     let that=this
-    // console.log(app.globalData.userInfo)
-    wx.getSetting({
-      success(res) {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-          wx.getUserInfo({
-            success: function (res) {
-              console.log(res.userInfo)
-              var avatarUrl = res.userInfo.avatarUrl
-              that.setData({
-                header: avatarUrl
-              })
-            }
-          })
-        }
-      }
-    })
+    // wx.getSetting({
+    //   success(res) {
+    //     if (res.authSetting['scope.userInfo']) {
+    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+    //       wx.getUserInfo({
+    //         success: function (res) {
+    //           console.log(res.userInfo)
+    //           var avatarUrl = res.userInfo.avatarUrl
+    //           that.setData({
+    //             header: avatarUrl
+    //           })
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
     that.setData({
       telnumber: app.username 
     })
+    console.log(that.data)
   },
   // bindGetUserInfo(e) {
   //   console.log(e.detail.userInfo)
