fba44debc08f687a506a968c0234f2cd20f2d7e6
zhaoqiangq <18235813489@163.com>
2019-08-21 09:35:57 +0800

    11111111

--- pages/webview/webview.js
+++ pages/webview/webview.js
@@ -13,9 +13,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      weburl: options.weburl
-    })
     var that =  this;
     wx.request({
       url: app.baseUrl + '/gzynew/orderinfo',
