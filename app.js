302d2390b57d2c2832aa2e910e497de74035e749
zhaoqiangq <18235813489@163.com>
2019-09-02 14:39:34 +0800

    小程序添加综合页

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ App({
               }else{
                 that.username = res.data.result.username;
                 that.usermob = res.data.result.usermob;
-                wx.switchTab({
-                  url: '/pages/index/index',
-                })
+                // wx.switchTab({
+                //   url: '/pages/index/index',
+                // })
               }
             }
           })
